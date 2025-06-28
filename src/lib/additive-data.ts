cef1a8586bea227a848864af2eb449a513aec34a
Prince Agyemang <princeagyemang094@gmail.com>
2025-06-28 15:22:06 +0000

    add to the new data file the GHGE for Asia to be 2.5 kg CO-eq and Europe

--- src/lib/additive-data.ts
+++ src/lib/additive-data.ts
@@ -20,7 +20,9 @@ export const feedAdditiveData = {
 export type FeedAdditiveData = typeof feedAdditiveData;
 
 export const regionalBaselineGHG = {
-    Canada: 2.3 // kg CO2e per kg live weight
+    Canada: 2.3, // kg CO2e per kg live weight
+    Asia: 2.5,
+    Europe: 2.8
 } as const;
 
 export type RegionalBaselineGHG = typeof regionalBaselineGHG;
